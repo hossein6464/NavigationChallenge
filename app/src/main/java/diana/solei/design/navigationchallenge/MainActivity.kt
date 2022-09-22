02bec926c93be24037316fad38473cd5ec2d0b39
hossein6464 <hossein.hajisoleimani@gmail.com>
2022-09-22 12:02:27 -0400

    Initial commit

---
+++ app/src/main/java/diana/solei/design/navigationchallenge/MainActivity.kt
@@ -0,0 +1,11 @@
+package diana.solei.design.navigationchallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
