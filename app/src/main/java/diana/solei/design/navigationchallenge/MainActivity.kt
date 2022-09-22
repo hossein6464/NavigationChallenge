14192e4e2f70597cdac28094f46bbdee5007dfb1
hossein6464 <hossein.hajisoleimani@gmail.com>
2022-09-22 12:08:04 -0400

    Added the necessary stuff for the Binding to the Main Activity and Main Activity Layout

--- app/src/main/java/diana/solei/design/navigationchallenge/MainActivity.kt
+++ app/src/main/java/diana/solei/design/navigationchallenge/MainActivity.kt
@@ -2,10 +2,14 @@ package diana.solei.design.navigationchallenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import diana.solei.design.navigationchallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainActivityBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainActivityBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
